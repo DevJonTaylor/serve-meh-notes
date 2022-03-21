434f4745291aafd10762ee4ef34535b290a20a65
Jon Taylor <jonnytest1101@icloud.com>
2022-03-21 09:21:30 -0500

    Setting up Heroku

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-global.__rootdir = __dirname;
-require = require("esm")(module)
-module.exports = require("./server.js")
+import express from 'express'
+import htmlRoutes from './routes/root'
+import notesApi from './routes/notes'
+import { viewEngineHtml } from './middleware'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3001
+
+express()
+  .use(express.static('public'))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(viewEngineHtml)
+  .use(cors({
+    origin: ['http://localhost:3000']
+  }))
+  .use('/api/notes', notesApi)
+  .use('/', htmlRoutes)
+  .listen(PORT, () => console.log(`http://localhost:${PORT}`));
