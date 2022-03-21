23ccb5ec1c8bff89041e07d3299605f064f12bc1
Jon Taylor <jonnytest1101@icloud.com>
2022-03-21 07:41:45 -0500

    Now items are able to be deleted

--- public/frontend/src/App.svelte
+++ public/frontend/src/App.svelte
@@ -1,15 +1,18 @@
 <script>
   import { onMount } from 'svelte'
   import { originalStore } from './lib/Store/Notes.js'
+  import { loadingStore } from './lib/Store/LoadingStore.js'
   import Navbar from './lib/Components/Navbar.svelte'
   import NoteTaker from './lib/Components/NoteTaker.svelte'
   import ListContainer from './lib/Components/ListContainer.svelte'
   import LoadingModal from './lib/Components/LoadingModal.svelte'
 
   onMount(() => {
+    loadingStore.set(true)
     fetch('http://localhost:3001/api/notes')
       .then(resp => resp.json())
       .then(notes => originalStore.set(notes))
+      .then(() => loadingStore.set(false))
   })
 
 
--- public/frontend/src/lib/Store/LoadingStore.js
+++ public/frontend/src/lib/Store/LoadingStore.js
@@ -1,5 +1,3 @@
 import { writable } from 'svelte/store'
 
 export const loadingStore = writable(false)
-export const messageFromServerStore = writable('')
-export const modalTitleStore = writable('')
